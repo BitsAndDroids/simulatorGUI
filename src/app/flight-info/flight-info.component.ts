7d6008c6186b85a5e84971ec952114bd91bb8b75
Dave Riedel <info@bitsanddroids.com>
2022-02-12 10:35:56 +0100

    Added TODO

--- src/app/flight-info/flight-info.component.ts
+++ src/app/flight-info/flight-info.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flight-info.component.scss']
 })
 export class FlightInfoComponent implements OnInit {
-
+  //The information displayed in this element is all hardcoded at the moment
+  //TODO create a flight info service to pass the data to relevant elements
   constructor() { }
 
   ngOnInit(): void {
